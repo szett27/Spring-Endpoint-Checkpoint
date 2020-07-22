18d1bdee3d3413abcc7378c2733e21fbc89c8bff
Scott <scott.zetterstrom@gmail.com>
2020-07-22 08:29:47 -0600

    Initial Commit

---
+++ src/main/java/com/example/SpringEndpointCheckpoint/SpringEndpointCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringEndpointCheckpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEndpointCheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEndpointCheckpointApplication.class, args);
+	}
+
+}
